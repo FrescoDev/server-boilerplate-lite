2ad1d8f17127a6aff0cc06536d4e81521bb06609
kojo-hinson <kojo.hinson@officeadminsmbp.home>
2018-12-22 17:01:52 +0000

    refactor:apply autoformatting

--- expectations/get-state.test.js
+++ expectations/get-state.test.js
@@ -1,11 +1,14 @@
-const test = require('ava');
-const request = require('supertest');
-const { server } = require('..');
+const test = require("ava");
+const request = require("supertest");
+const { server } = require("..");
 
-test('When a client submits a GET request to the /state endpoint', async ({ is, plan }) => {
+test("When a client submits a GET request to the /state endpoint", async ({
+  is,
+  plan
+}) => {
   plan(2);
 
-  const response = await request(server).get('/state');
+  const response = await request(server).get("/state");
 
   const expectedStatus = 200;
 
