6dfbf4aaf3cc8927e84b696d195df5e269741729
kojo-hinson <kojo.hinson@officeadminsmbp.home>
2018-12-23 11:27:44 +0000

    refactor (server): Add error handling upon init, redact auth headers from log stream.

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const server = require("fastify")({
+  redact: ['req.headers.authorization'],
   logger: true
 });
 
 server.get("/state", (request, reply) => {
-  reply.send({ isResponsive: true });
+  return reply.send({ isResponsive: true });
 });
 
 server.listen(process.env.PORT || 3000, "0.0.0.0", (err, address) => {
-  if (err) throw err;
+  if (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+
   server.log.info(`server listening on ${address}`);
 });
 
